aa80c8f8499859399699805449b5178f36015703
Luis Colunga Perez <luis@karmapulse.com>
2024-09-12 15:38:12 -0600

    Add husky, eslint and prettier configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,12 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import eslintPrettier from 'eslint-plugin-prettier/recommended';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
+  eslintPrettier,
   { ignores: ['dist'] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
@@ -25,4 +27,4 @@ export default tseslint.config(
       ],
     },
   },
-)
+);
---
+++ prettier.config.mjs
@@ -0,0 +1,15 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+
+export default {
+  tabWidth: 2,
+  semi: true,
+  printWidth: 80,
+  bracketSpacing: true,
+  bracketSameLine: true,
+  jsxSingleQuote: true,
+  singleQuote: true,
+  trailingComma: 'all',
+};
