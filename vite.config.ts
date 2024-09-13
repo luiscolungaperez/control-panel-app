d1b64cdf66283ff67357acce77b3c555a0e054e8
Luis Colunga Perez <luis@karmapulse.com>
2024-09-12 18:22:52 -0600

    Add testing configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
+/// <reference types="vitest" />
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    setupFiles: ['./jest-setup.ts'],
+  },
 });
