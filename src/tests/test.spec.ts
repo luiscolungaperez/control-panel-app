b8bb6e26fedd7bfd8ef79ea15a4fcef307b9f9c4
Luis Colunga Perez <luis@karmapulse.com>
2024-09-13 00:13:19 -0600

    Update test folders

--- src/tests/test.spec.ts
+++
@@ -1,7 +0,0 @@
-import { describe, expect, test } from 'vitest';
-
-describe('First test', () => {
-  test('Should return 10 when the input values are 5 + 5 ', () => {
-    expect(5 + 5).toBe(10);
-  });
-});
