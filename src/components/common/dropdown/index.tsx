4720dbc524e7e1f5f50169b962189d4288c9f4ce
Luis Colunga Perez <luis@karmapulse.com>
2024-09-16 14:07:05 -0600

    Add dropdown refactor

--- src/components/common/dropdown/index.tsx
+++ src/components/common/dropdown/index.tsx
@@ -1,56 +1,60 @@
 import clsx from 'clsx';
-import { useState } from 'react';
+import { useDropdown } from './hook';
 import styles from './styles.module.css';
 
 interface Props {
-  placeholder: string;
+  label?: string;
   value?: string | number;
   options: Dropdown.Option[];
-  onClick: (value: string | number) => void;
+  onClick: (value?: string | number) => void;
 }
 
 export const Dropdown: React.FC<Props> = ({
   options,
-  placeholder = 'Select one option',
+  label,
   value,
   onClick,
 }) => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const handleIsOpen = () => setIsOpen((value) => !value);
-
-  const handleClick = (value: string | number) => {
-    onClick(value);
-    handleIsOpen();
-  };
+  const { handleClick, isOpen, wrapperRef, handleIsOpen } =
+    useDropdown(onClick);
 
   return (
-    <div className={styles.dropdown}>
-      <label htmlFor={`dropdown-${value || 'simple'}`}>{placeholder}</label>
-      <button
-        id={`dropdown-${value || 'simple'}`}
-        name={`dropdown-${value || 'simple'}`}
-        className={styles['dropdown-btn']}
-        onClick={handleIsOpen}
-        aria-expanded={isOpen}>
-        {options.find((options) => options.value === value)?.label ||
-          placeholder}
-      </button>
-      {isOpen && (
-        <div
-          role='listitem'
-          className={clsx(styles['dropdown-content'], styles.show)}>
-          {options?.map((option, index) => (
+    <>
+      <div ref={wrapperRef} className={styles.dropdown}>
+        {label && (
+          <label htmlFor={`dropdown-${value || 'simple'}`}>{label}:</label>
+        )}
+        <button
+          id={`dropdown-${value || 'simple'}`}
+          name={`dropdown-${value || 'simple'}`}
+          className={styles['dropdown-btn']}
+          onClick={handleIsOpen}
+          aria-expanded={isOpen}>
+          {options.find((options) => options.value === value)?.label ||
+            'Select a option'}
+        </button>
+        {isOpen && (
+          <div
+            role='listitem'
+            className={clsx(styles['dropdown-content'], styles.show)}>
             <button
               type='button'
               className={styles.button}
-              onClick={() => handleClick(option.value)}
-              key={`${option.value}-${index}`}>
-              {option.label}
+              onClick={() => handleClick(undefined)}>
+              Clear options
             </button>
-          ))}
-        </div>
-      )}
-    </div>
+            {options?.map((option, index) => (
+              <button
+                type='button'
+                className={styles.button}
+                onClick={() => handleClick(option.value)}
+                key={`${option.value}-${index}`}>
+                {option.label}
+              </button>
+            ))}
+          </div>
+        )}
+      </div>
+    </>
   );
 };
